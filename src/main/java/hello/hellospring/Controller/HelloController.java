a4c31b35b20ef2d565a6dd5af4e9d71560ed0cc2
jh950925 <jh950925@naver.com>
2022-02-02 18:43:16 +0900

    2022_02_02 API

--- src/main/java/hello/hellospring/Controller/HelloController.java
+++ src/main/java/hello/hellospring/Controller/HelloController.java
@@ -33,7 +33,7 @@ public class HelloController {
     public Hello helloApi(@RequestParam("name") String name){
         Hello hello = new Hello();
         hello.setName(name);
-        return hello;
+        return hello; //
 
     }
 
