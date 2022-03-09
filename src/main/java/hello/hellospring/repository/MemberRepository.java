2f5486eb7a8f1f7bfef5934f1515d30027e1df30
jh950925 <jh950925@naver.com>
2022-03-09 18:48:19 +0900

    회원서비스 테스트_2022_03_09

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member); //save에 저장하여 밑에서 Optional로 받아오기
+
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+}
