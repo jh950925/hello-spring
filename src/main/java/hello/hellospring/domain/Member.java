e09e472a56571753432a77bc9cc237897ec33eb9
KimJunHyeong <jh950925@naver.com>
2022-03-26 16:33:19 +0900

    JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,16 +1,21 @@
 package hello.hellospring.domain;
 
+
+import org.hibernate.annotations.Generated;
+
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
 
-    public Long getId() {
-        return id;
-    }
-    public String getName(){
-        return name;
-    }
+    public Long getId() {return id;}
+    public String getName(){return name;}
 
     public void setId(Long id) {
         this.id = id;
