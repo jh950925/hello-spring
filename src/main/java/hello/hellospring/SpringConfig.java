829660bd7e13ac54e96eabc455f8788408175bc8
KimJunHyeong <jh950925@naver.com>
2022-03-28 21:55:39 +0900

    AOP적용_에러 중단

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring;
 
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,11 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop TimeTraceAop(){
+//        return new TimeTraceAop();
+//    }
 
 //    @Bean
 //    public MemberRepository memberRepository() {
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -26,11 +26,12 @@ public class MemberService {
 
     /** 회원가입 **/
     public Long join(Member member){
-        //같은 이름이 있는 중복 회원X
 
+        //같은 이름이 있는 중복 회원X
         validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -42,7 +43,7 @@ public class MemberService {
 
     /** 전체 회원 조회 **/
     public List<Member> findMember(){
-        return memberRepository.findAll();
+            return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId){
